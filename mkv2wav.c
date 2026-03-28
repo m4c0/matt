1929d4a40353fb2c64c4cc73bcfb9481a494d3db
Eduardo Costa <m4c0@users.noreply.github.com>
2026-03-28 19:30:54 +0000

    mkv2wav: reading EBML Header

--- mkv2wav.c
+++ mkv2wav.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #define ERR(...) fprintf(stderr, __VA_ARGS__)
 #define ASSERT(x, ...) if (!(x)) { ERR(__VA_ARGS__); return 0; }
@@ -39,13 +40,21 @@ int vint_raw(FILE * f, uint64_t * res, int id) {
 }
 int vint(FILE * f, uint64_t * res) { return vint_raw(f, res, 0); }
 
-int element(FILE * f, uint64_t exp_elid) {
-  uint64_t elid, elsz;
+int element(FILE * f, uint64_t exp_elid, uint64_t * elsz) {
+  uint64_t elid;
+
   ASSERT(vint_raw(f, &elid, 1), " reading Element ID");
   ASSERT(elid && ~elid, "Element ID cannot have all zeroes or all ones");
-  ASSERT(vint(f, &elsz), " reading Element Data Size");
-  ASSERT(~elid, "Element Data Size cannot have all ones");
-  ASSERT(elid == exp_elid, "Invalid Element ID");
+  ASSERT(elid == exp_elid, "Invalid element ID (exp %llx got %llx)", exp_elid, elid);
+
+  ASSERT(vint(f, elsz), " reading Element Data Size");
+  ASSERT(~*elsz, "Element Data Size cannot have all ones");
+  return 1;
+}
+int element_sized(FILE * f, uint64_t exp_elid, uint64_t exp_elsz) {
+  uint64_t elsz;
+  ASSERT(element(f, exp_elid, &elsz), "");
+  ASSERT(elsz == exp_elsz, "Invalid element data size (exp %lld got %lld)", exp_elsz, elsz);
   return 1;
 }
 
@@ -60,25 +69,41 @@ int run(const char * name) {
   FILE * f = fopen(name, "rb");
   ASSERT(f, "file not found");
 
-  ASSERT(element(f, 0x1A45DFA3), " reading EBML Element ID");
-  ASSERT(element(f, 0x4286), " reading EBML Version ID");
+  uint64_t hdr_sz;
+  ASSERT(element(f, 0x1A45DFA3, &hdr_sz), " reading EBML Element ID");
+  long l = ftell(f);
+
+  ASSERT(element_sized(f, 0x4286, 1), " reading EBML Version ID");
   ASSERT(check_u8(f, 1), " of EBML Version ID");
-
-  ASSERT(element(f, 0x42F7), " reading EBML Read Version ID");
+  ASSERT(element_sized(f, 0x42F7, 1), " reading EBML Read Version ID");
   ASSERT(check_u8(f, 1), " of EBML Read Version ID");
-
-  ASSERT(element(f, 0x42F2), " reading EBML Max ID Length");
+  ASSERT(element_sized(f, 0x42F2, 1), " reading EBML Max ID Length");
   ASSERT(check_u8(f, 4), " of EBML Max ID Length");
-
-  ASSERT(element(f, 0x42F3), " reading EBML Max Size Length");
+  ASSERT(element_sized(f, 0x42F3, 1), " reading EBML Max Size Length");
   ASSERT(check_u8(f, 8), " of EBML Max Size Length");
+
+  uint64_t len;
+  ASSERT(element(f, 0x4282, &len), " reading DocType");
+  ASSERT(len == 8, "Invalid DocType length (exp 8 got %lld)", len);
+  char buf[8];
+  ASSERT(fread(buf, 8, 1, f), "Error reading DocType");
+  ASSERT(0 == strncmp(buf, "matroska", 8), "Invalid DocType (exp 'matroska' got '%8s')", buf);
+
+  ASSERT(element_sized(f, 0x4287, 1), " reading DocType Version ID");
+  ASSERT(check_u8(f, 4), " of DocType Version ID");
+  ASSERT(element_sized(f, 0x4285, 1), " reading DocType Read Version ID");
+  ASSERT(check_u8(f, 2), " of DocType Read Version ID");
+
+  ASSERT(hdr_sz == ftell(f) - l, "Unsupported elements in EBML Header");
+
+  ASSERT(element(f, 0x18538067, &hdr_sz), " reading Segment");
 
   return 1;
 }
 
 int main(int argc, char ** argv) {
   for (int i = 1; i < argc; i++) {
-    ASSERT(run(argv[i]), " while reading %s", argv[i]);
+    ASSERT(run(argv[i]), " of file %s", argv[i]);
   }
   if (argc == 1) {
     fprintf(stderr, "usage: %s <file.mkv>...\n", argv[0]);
